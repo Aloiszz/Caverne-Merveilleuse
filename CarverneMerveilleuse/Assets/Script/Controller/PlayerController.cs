3ac251683fb67920abc44c2cba4ea3ec7fdad99b
Aloisz <a.tupinon@rubika-edu.com>
2022-09-13 17:34:09 +0200

    on fait des chose mais pas suêr

--- CarverneMerveilleuse/Assets/Script/Controller/PlayerController.cs
+++ CarverneMerveilleuse/Assets/Script/Controller/PlayerController.cs
@@ -4,14 +4,49 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed;
+    private Rigidbody2D rb;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
+    {
+        GameMove();
+    }
+
+    private void GameMove()
+    {
+        if (Input.GetKey(KeyCode.Z))
+        {
+            //transform.Translate(Vector3.up * speed * Time.deltaTime);
+            rb.AddForce(Vector2.up * speed);
+        }
+
+        if (Input.GetKey(KeyCode.Q))
+        {
+            //transform.Translate(Vector3.left * speed * Time.deltaTime);
+            rb.AddForce(Vector2.left * speed);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            //transform.Translate(Vector3.down * speed * Time.deltaTime);
+            rb.AddForce(Vector2.down * speed);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            //transform.Translate(Vector3.right * speed * Time.deltaTime);
+            rb.AddForce(Vector2.right * speed);
+        }
+    }
+
+    private void Shoot()
     {
         
     }
