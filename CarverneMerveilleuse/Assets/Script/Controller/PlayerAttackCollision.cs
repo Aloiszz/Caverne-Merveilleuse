9bf8566c631d14fd802226a9c0811be24c401ea4
Aloisz <a.tupinon@rubika-edu.com>
2022-09-19 15:36:13 +0200

    on bosse les combats

--- CarverneMerveilleuse/Assets/Script/Controller/PlayerAttackCollision.cs
+++ CarverneMerveilleuse/Assets/Script/Controller/PlayerAttackCollision.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,6 +7,7 @@ public class PlayerAttackCollision : MonoBehaviour
 {
     public SpriteRenderer sprite;
     public Collider2D coll;
+    public GameObject pivot;
     
     [Header("Singleton")]
     public static PlayerAttackCollision instance;
@@ -26,8 +28,28 @@ public class PlayerAttackCollision : MonoBehaviour
         sprite = GetComponent<SpriteRenderer>();
         coll = GetComponent<Collider2D>();
         
-        sprite.enabled = false;
+        sprite.enabled = true;
         coll.enabled = false;
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        difference.Normalize();
+        float rotationZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+        pivot.transform.rotation = Quaternion.Euler(0f, 0f, rotationZ);
+ 
+        if (rotationZ < -90 || rotationZ > 90)
+        {
+            if(PlayerController.instance.gameObject.transform.eulerAngles.y == 0)
+            {
+                pivot.transform.localRotation = Quaternion.Euler(180, 0, -rotationZ);
+            } 
+            /*else if (PlayerController.instance.gameObject.transform.eulerAngles.y == 180) 
+            {
+                pivot.transform.localRotation = Quaternion.Euler(180, 180, -rotationZ);
+            }*/
+        }
     }
 
     // Update is called once per frame
@@ -35,7 +57,7 @@ public class PlayerAttackCollision : MonoBehaviour
     {
         if (PlayerController.instance.playerSO.isStriking)
         {
-            //
+            
         }
     }
 }
