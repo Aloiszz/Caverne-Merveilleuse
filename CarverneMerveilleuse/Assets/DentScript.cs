48f7eb395b4ccadce700c4fcff00b2b66912b5df
Maxime <m.pawelski@rubika-edu.com>
2022-10-10 18:07:30 +0200

    les ennemis drop des dents (fin des carrés mais tkt)

---
+++ CarverneMerveilleuse/Assets/DentScript.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DentScript : MonoBehaviour
+{
+    public PlayerController player; 
+    public UIManager ui;
+
+    private void Start()
+    {
+        player = GameObject.FindWithTag("Player").GetComponent<PlayerController>();
+        ui = GameObject.Find("UIManager").GetComponent<UIManager>();
+    }
+
+    private void Update()
+    {
+        if ((player.transform.position - transform.position).magnitude <= 2)
+        {
+            transform.position = Vector2.MoveTowards(gameObject.transform.position, player.transform.position, 5);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            ui.money += 1;
+            Destroy(gameObject);
+        }
+    }
+}
