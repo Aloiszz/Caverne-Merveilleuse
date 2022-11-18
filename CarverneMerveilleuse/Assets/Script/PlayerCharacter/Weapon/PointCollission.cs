5eff640a9fce8e7a80df294cdf319ab8f2f641a2
Aloisz <a.tupinon@rubika-edu.com>
2022-11-18 10:59:57 +0100

    ThrowAttack

--- CarverneMerveilleuse/Assets/Script/PlayerCharacter/Weapon/PointCollission.cs
+++ CarverneMerveilleuse/Assets/Script/PlayerCharacter/Weapon/PointCollission.cs
@@ -1,18 +1,98 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PointCollission : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Collider2D coll;
+    public Rigidbody2D rb;
+    public int bounceInt = 2;
+
+    public static PointCollission instance;
+    
+    private void Awake()
     {
+        if (instance != null && instance != this) 
+        { 
+            Destroy(this); 
+        } 
+        else 
+        { 
+            instance = this; 
+        } 
+    }
+    
+    private void Start()
+    {
+        coll = GetComponent<Collider2D>();
+        rb = GetComponent<Rigidbody2D>();
         
+        coll.enabled = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        if (verif)
+        {
+            BounceWeapon();
+        }
+
+        if (transform.position == PlayerThrowAttack.instance.points[bounceInt])
+        {
+            verif = false;
+        }
+    }
+
+    private bool verif;
+    [HideInInspector] public bool verifPremierTouch;
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.layer == 9)
+        {
+            verif = true;
+            verifPremierTouch = true;
+            Debug.Log(transform.position.x + "" + transform.position.y);
+        }
         
+    }
+    
+    void BounceWeapon()
+    {
+        rb.velocity = Vector3.zero;
+        rb.angularVelocity = 0;
+        //rb.AddForce((PlayerThrowAttack.instance.points[bounceInt]), ForceMode2D.Impulse);
+        
+        transform.position = Vector3.MoveTowards(transform.position,
+            PlayerThrowAttack.instance.points[bounceInt], Time.deltaTime * 
+                                                          PlayerThrowAttack.instance.ThrowSpeed[PlayerThrowAttack.instance.ThrowDamageIndex] );
+
+        //bounceInt++;
+    }
+    
+    public void ThrowWeapon()
+    {
+        //rb.AddForce((PlayerAttackCollision.instance.difference) * 20, ForceMode2D.Impulse);
+        
+        transform.position = Vector3.MoveTowards(transform.position,
+            PlayerThrowAttack.instance.points[1], Time.deltaTime * 
+                                                  PlayerThrowAttack.instance.ThrowSpeed[PlayerThrowAttack.instance.ThrowDamageIndex]);
+        
+        /*transform.position = Vector3.MoveTowards(transform.position,
+            PlayerAttackCollision.instance.difference, Time.deltaTime * 40);*/
+        
+        //gameObject.transform.DORotate(new Vector3(0, 0, 2160), 3);
+    }
+    
+    public void IsWeaponActive(bool verif)
+    {
+        if (verif)
+        {
+            coll.enabled = true;
+        }
+        else
+        {
+            coll.enabled = false;
+        }
     }
 }
