6d2a9598b2c36e6723166aacd539837fab1c7914
Maxime <m.pawelski@rubika-edu.com>
2022-11-22 11:26:53 +0100

    ah en fait non

--- CarverneMerveilleuse/Assets/Script/Gen Pro/RoomGeneration/RoomSpawnerV2.cs
+++ CarverneMerveilleuse/Assets/Script/Gen Pro/RoomGeneration/RoomSpawnerV2.cs
@@ -64,10 +64,6 @@ public class RoomSpawnerV2 : MonoBehaviour
     {
         if (col.CompareTag("Player"))
         {
-            if (isShopDoor)
-            {
-                ShopScript.instance.OnExit();
-            }
             ProceduralGeneration(); //Entre en contact avec une porte
         } 
     }
