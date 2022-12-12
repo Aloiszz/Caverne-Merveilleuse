55198691e314de01902f7ccba4355e1053acb700
Maxime <m.pawelski@rubika-edu.com>
2022-12-12 16:11:49 +0100

    on peut dash à travers les trous (mais il y a pas encore de vrai trou)

---
+++ CarverneMerveilleuse/Assets/trouScript.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class trouScript : MonoBehaviour
+{
+    private Vector2 actualVelocity;
+    private Rigidbody2D playerRB;
+    private Collider2D coll;
+
+    private void Start()
+    {
+        playerRB = PlayerController.instance.gameObject.GetComponent<Rigidbody2D>();
+        coll = GetComponent<Collider2D>();
+    }
+
+    private void Update()
+    {
+        Debug.Log(PlayerController.instance.isInHole);
+        if (PlayerController.instance.isDashing)
+        {
+            coll.isTrigger = true;
+            
+        }
+        else
+        {
+            coll.isTrigger = false;
+        }
+
+        if (PlayerController.instance.isInHole)
+        {
+            PlayerController.instance.dashTrail.SetActive(true);
+            PlayerController.instance.isDashing = true;
+            playerRB.velocity = PlayerController.instance.actualVelocity;
+        }
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            Debug.Log("je rentre");
+            PlayerController.instance.isInHole = true;
+            PlayerController.instance.actualVelocity = playerRB.velocity.normalized * new Vector2(50, 50);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("je sors");
+            PlayerController.instance.dashTrail.SetActive(false);
+            PlayerController.instance.isInHole = false;
+            PlayerController.instance.dashTrail.SetActive(false);
+            PlayerController.instance.isDashing = false;
+        }
+    }
+}
