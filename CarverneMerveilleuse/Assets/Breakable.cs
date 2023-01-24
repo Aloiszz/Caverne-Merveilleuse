64dda028e06018650602f511ed3ad18ba683a5b1
Maxime <m.pawelski@rubika-edu.com>
2023-01-24 19:57:19 +0100

    pour pas gagner d'argent avec les pots du hub

--- CarverneMerveilleuse/Assets/Breakable.cs
+++ CarverneMerveilleuse/Assets/Breakable.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using DG.Tweening;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
@@ -9,6 +10,7 @@ public class Breakable : MonoBehaviour
 {
     public GameObject dent;
     public ParticleSystem eclat;
+    public bool cantDrop;
     private void OnTriggerEnter2D(Collider2D col)
     {
         if (col.gameObject.layer == 8)
@@ -17,18 +19,21 @@ public class Breakable : MonoBehaviour
             Instantiate(eclat, transform.position, Quaternion.identity,
                 RoomManager.instance.roomMemory[RoomManager.instance.roomMemoryIndex].transform);
             int drop = Random.Range(0, 10);
-            if (drop > 4 && drop <= 8)
+            if (!cantDrop)
             {
-                
-                gameObject.transform.DOMove(new Vector3(Random.Range(-3, 4), Random.Range(-3, 4)), 0.1f);
-                Instantiate(dent, gameObject.transform.position, Quaternion.identity, transform.parent);
-            }
-            else if (drop > 8)
-            {
-                for (int i = 0; i < 2; i++)
+                if (drop > 4 && drop <= 8)
                 {
+                
                     gameObject.transform.DOMove(new Vector3(Random.Range(-3, 4), Random.Range(-3, 4)), 0.1f);
                     Instantiate(dent, gameObject.transform.position, Quaternion.identity, transform.parent);
+                }
+                else if (drop > 8)
+                {
+                    for (int i = 0; i < 2; i++)
+                    {
+                        gameObject.transform.DOMove(new Vector3(Random.Range(-3, 4), Random.Range(-3, 4)), 0.1f);
+                        Instantiate(dent, gameObject.transform.position, Quaternion.identity, transform.parent);
+                    }
                 }
             }
             AudioManager.instance.SFXSource.PlayOneShot(AudioManager.instance.potsClips[random]);
