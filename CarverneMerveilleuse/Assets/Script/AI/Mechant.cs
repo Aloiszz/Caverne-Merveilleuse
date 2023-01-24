ef02ec06dc13e09098c885e7363d777dc1993ff7
Aloisz <a.tupinon@rubika-edu.com>
2023-01-24 22:56:20 +0100

    Merci l'EKIP <3 !!!!

--- CarverneMerveilleuse/Assets/Script/AI/Mechant.cs
+++ CarverneMerveilleuse/Assets/Script/AI/Mechant.cs
@@ -164,6 +164,7 @@ public class Mechant : MonoBehaviour
             {
                 Score.instance.score += pointScore;
                 Score.instance.AddScore();
+                enabled = false;
             }
             
         }
